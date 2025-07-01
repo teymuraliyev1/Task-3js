0568dcac7a71f7add9c777600240ef942d048a53
teymuraliyev1 <118759224+teymuraliyev1@users.noreply.github.com>
2025-07-01 16:59:34 +0400

    init

--- jstasks/task1.js
+++
@@ -1,7 +0,0 @@
-
-function findSum(a, b) {
-    let sum = a + b;
-    console.log("Sum:", sum);
-  }
-  
- findSum(5, 8); 
--- jstasks/task2.js
+++
@@ -1,7 +0,0 @@
-let countries = ["America", "France", "Turkey", "Russia", "China", "Vietnam", "Azerbaijan"];
-
-countries.forEach(country => {
-  if (country.length > 6) {
-    console.log(country);
-  }
-});
